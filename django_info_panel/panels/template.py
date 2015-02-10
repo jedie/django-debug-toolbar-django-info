d3ab77c8e816c55d50324497e310b08976b321b0
JensDiemer <git@jensdiemer.de>
2015-02-10 18:02:39 +0100

    Add "Template Tags/Filters Information" with a list of all registered template tags/filters

---
+++ django_info_panel/panels/template.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+"""
+    django-info-panel
+    ~~~~~~~~~~~~~~~~~
+
+    :copyleft: 2015 by the django-debug-toolbar-django-info team, see AUTHORS for more details.
+    :created: 2015 by JensDiemer.de
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import, unicode_literals
+import fnmatch
+import glob
+
+import sys
+import os
+
+if __name__ == "__main__":
+    # run directly
+    sys.path.insert(0, os.path.expanduser("~/page_instance"))
+    os.environ["DJANGO_SETTINGS_MODULE"] = "example_project.settings"
+
+import inspect
+import logging
+
+import django
+from django.db import backend, connection
+from django.db.models.loading import get_apps, get_models
+from django.template import get_templatetags_modules, get_library
+from django.utils.translation import ugettext_lazy as _
+from django.template import RequestContext, Template
+
+
+from debug_toolbar.panels import Panel
+
+logger = logging.getLogger(__name__)
+
+
+class TemplateTagInfo(object):
+    def __init__(self, name):
+        name=name
+
+def collect_docs(items):
+    data = {}
+    for item in items:
+        doc = inspect.getdoc(items[item]) or ""
+        if doc:
+            doc = inspect.cleandoc(doc)
+        data[item] = doc
+    return data
+
+
+
+def get_templateinfo():
+    modules = get_templatetags_modules()
+
+    template_info = {}
+    for module in modules:
+        try:
+            templatetag_mod = __import__(module, {}, {}, [''])
+        except ImportError:
+            continue
+
+        mod_path = os.path.dirname(inspect.getabsfile(templatetag_mod))
+        tag_files = [
+            os.path.splitext(fn)[0]
+            for fn in os.listdir(mod_path)
+            if fnmatch.fnmatch(fn, "*.py") and not fn.startswith("_")
+        ]
+
+        lib_info = {}
+        for taglib in tag_files:
+            try:
+                lib = get_library(taglib)
+            except:
+                continue
+
+            lib_info[taglib] = {
+                "tags": collect_docs(lib.tags),
+                "filters": collect_docs(lib.filters),
+            }
+
+        template_info[module.split(".")[0]] = lib_info
+
+    return template_info
+
+
+
+
+class TemplateInfo(Panel):
+    nav_title = _("Template Info")
+    title=_("Template Tags/Filters Information")
+    template = 'django_info/panels/template.html'
+
+    def process_response(self, request, response):
+        self.record_stats({
+            "template_info": get_templateinfo(),
+        })
+
+
+if __name__=="__main__":
+    from pprint import pprint
+    django.setup()
+    template_info = get_templateinfo()
+
+    pprint(template_info)
